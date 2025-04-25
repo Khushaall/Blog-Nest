bcc96c27acdc62810987973696019c8ff100fbe7
Khushaall <135150397+Khushaall@users.noreply.github.com>
2025-04-25 13:24:27 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,10 +5,10 @@ const mongoURI = process.env.MONGO_URI;
 
 
 mongoose.connect(mongoURI)
-.then(() => console.log('MongoDB connected successfully'))
-.catch(err => console.error('MongoDB connection error:', err));
+// .then(() => console.log('MongoDB connected successfully'))
+// .catch(err => console.error('MongoDB connection error:', err));
 
-mongoose.connect("mongodb+srv://khushalsharma122:UB0irbMOXbBAK4QK@cluster0.v8rvv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+// mongoose.connect("mongodb+srv://khushalsharma122:UB0irbMOXbBAK4QK@cluster0.v8rvv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 
 const userSchema= mongoose.Schema({
